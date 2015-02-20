24fa2a50c40c69b7dfc91b33763f5402f8cb63d8
Chloe Bradley <ccbradley77@gmail.com>
2015-02-20 13:43:35 -0700

    validation excersize

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,11 @@
 class Person < ActiveRecord::Base
+  validates :first_name, presence: true, length:{minimum:1}
+  validates :last_name, presence: true, length: {minimum:1}
+  validates :date_of_birth, presence: true
+  validate :validate_special_characters_dont_exist
 
 
-
+  def validate_special_characters_dont_exist
+    
+  end
 end
